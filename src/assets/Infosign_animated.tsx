20dc91090618052d58ef35cdab5aaf471aeb1b3d
apl03T2 <86350267+apl03T2@users.noreply.github.com>
2022-03-03 14:32:21 +0100

    infosignage

---
+++ src/assets/Infosign_animated.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Infosign" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="Infosign.png" width="400" height="400"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="1000"/>
+   <frame tileid="13" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="500"/>
+   <frame tileid="15" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
